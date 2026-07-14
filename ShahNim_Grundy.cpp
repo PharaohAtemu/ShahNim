70e190e0855d43b95ea7a3f9e6a08a20e6056b19
PharaohAtemu <mrspock06@gmail.com>
2026-07-14 15:41:06 -0400

    Add files via upload
    updated Grundy.cpp to ask for and account for multiple piles with different values

--- ShahNim_Grundy.cpp
+++ ShahNim_Grundy.cpp
@@ -13,6 +13,17 @@ vector<int> p_position = {1,3};
 int MAX_GRUNDY = 1;
 
 void findMex(int n);
+int multiple(vector<int> piles);
+
+int mulitple(vector<int> piles){
+    int result = 0;
+
+    for(int i = 0; i < piles.size(); i++){
+        result ^= piles[i];
+    }
+
+    return result;
+}
 
 void findMex(int n){
 
@@ -57,20 +68,60 @@ void findMex(int n){
 
 int main(){
     int MAX = 0;
+    int piles = 0;
+    int exit = 1;
+    int search = 0;
+    vector<int> game;
 
-    cout<<"Enter number of stones:";
-    cin>> MAX;
+    cout<<"Enter number of piles: ";
+    cin>> piles; 
+
+    game.resize(piles);
+
+    for(int i = 0; i < piles; i++){
+        cout<<"Enter number of stones in pile "<< i+1<<": ";
+        cin>> game[i];
+
+        //update the max number
+        if(game[i] > MAX){
+            MAX = game[i];
+        }
+    }
     
-    
+
+    //calculate the grundy numbers we need
     for(int i = 4; i <= MAX; i++){
         findMex(i);
     }
 
-    for(int i = 0; i<= MAX; i++){
-        cout<< i << " mex = "<< grundy[i]<< "\n";
+    if(piles > 1){
+        if(mulitple(game) != 0)
+        {
+            cout<< "First player wins this game.\n";
+        }else{
+            cout<<"Second player wins this game.\n";
+        }
     }
 
-    cout<< "Highest Grundy number is: "<<MAX_GRUNDY;
+    cout<< "Highest Grundy number is: "<<MAX_GRUNDY<<"\n";
+
+    do{
+        do{
+            cout<<"Which Grundy value up to "<< MAX<<" do you want to know?: ";
+            cin>>search;
+        }while(search > MAX);
+
+        if(search == -1){
+            for(int i = 0; i < MAX-2; i++){
+                cout<<grundy[i]<<" "<<grundy[i+1]<<" "<<grundy[i+2]<<"\n";
+            }
+        }else{
+            cout<<grundy[search]<<"\n";
+        }
+
+        cout<<"Do you wish to exit? 1 for no, 2 for yes: ";
+        cin>> exit;
+    }while(exit == 1);
 
     return 0;
 }
